adeea7aca7324bb3c790bcffe5254ceda6849cb2
fly <luxiaofeng006@qq.com>
2026-05-09 16:20:54 +0800

    5

--- t.cs
+++ t.cs
@@ -16,19 +16,3 @@ catch (Exception ex)
 }
 
 Console.WriteLine("4");
-
-Thread.Sleep(5000);
-
-
-var builder = WebApplication.CreateBuilder(args);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-app.Map("/", (HttpContext context) =>
-{
-    return context.Request.Path + context.Request.QueryString.Value + ">" + string.Join("; ", context.Request.Headers.Select(p => $"{p.Key}: {p.Value}"));
-});
-
-app.Run();
