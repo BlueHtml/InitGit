f227bc8ced81c5d2eaec64e43349d5d8e2f0bdda
fly <luxiaofeng006@qq.com>
2026-05-09 12:40:41 +0800

    init

---
+++ t.cs
@@ -0,0 +1,13 @@
+﻿
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.Map("/", (HttpContext context) =>
+{
+    return context.Request.Path + context.Request.QueryString.Value + ">" + string.Join("; ", context.Request.Headers.Select(p => $"{p.Key}: {p.Value}"));
+});
+
+app.Run();
